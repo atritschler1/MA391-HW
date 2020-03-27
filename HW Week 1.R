ca34c0ec32e2385a0dde94ed4d98e60dc8b267d6
atritschler1 <46549842+atritschler1@users.noreply.github.com>
2020-03-27 04:50:40 -0400

    Add files via upload

---
+++ HW Week 1.R
@@ -0,0 +1,31 @@
+### Ch. 7 HW ###
+
+# Number 5 #
+#(a).
+2050/12
+#170.8333 ~ 171
+
+#(b).
+#(n/171) +- (2sqrt(n)/171) *12
+147*12
+199*12
+#1764-2388
+
+#(c).
+#(171/lambda) +- (2sqrt(171)/lambda)
+
+#(d).
+#no idea, couldn't find in textbook
+
+
+# Number 6 #
+#(b).
+dpois(18,171)
+
+
+# Number 11 #
+.70^16
+
+prob = c(0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1)
+res = prob^16
+print(res)
